be328da9b2ea5273346618e96070b544383aaf76
Yamile <yamilechamorro11@gmail.com>
2024-01-20 08:31:10 -0500

    Scrpt calculadora basica

---
+++ basic_calc.py
@@ -0,0 +1,26 @@
+'''Script title: Basic Calculator v1
+Developer name: Yamile Chamorro
+Date: 18-01-2024'''
+
+'''Description:
+This calculator let you get the result of 4 basic math operations from two numbers (Variables)'''
+
+#INPUTS (two statics variables)
+num1 = 20
+num2 = 5
+
+#PROCESOS
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+#OUTPUTS
+print ("Version 1")
+print ("Number 1 : ", num1, type(num1))
+print ("Number 2 : ", num2, type(num2))
+print ("the addition is: ", add, type(add))
+print ("the substraction is: ", sub, type(sub))
+print ("the multiplication is: ", mul, type(mul))
+print ("the division is: ", div, type(div))
+print ("\n")
