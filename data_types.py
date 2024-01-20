9301c1fa8ac2be47a264e04818368ad7c55acb62
Yamile <yamilechamorro11@gmail.com>
2024-01-20 12:06:41 -0500

    First commit data_types

---
+++ data_types.py
@@ -0,0 +1,50 @@
+#Tipos de datos en Python
+#Python es un lenguaje dinamicamente tipado
+
+#1. Númericos
+##Enteros Z --> int
+num1 = 42
+print( "Num1: ", type (num1))
+##Flotantes o decimales (Reales) => Float
+num2 = 50.5
+print ("Num2: ", type(num2))
+##Complejos => Complex
+num3 = 2j
+print ("Num3: ", type(num3))
+
+#2. Cadena
+my_name = "Yamile"
+print ("my_name", type (my_name))
+my_lastname = "Chamorro"
+print ("my_lastname", type (my_lastname))
+profile = "Yamile es una persona disiplinada"
+print ("profile", type (profile))
+address =  'Carrera 8 # 13-42'
+print ("address", type (address))
+
+a = 1
+b = 1
+suma1 = a + b #Suma Aritmetica
+
+c = '1'
+d = '1'
+suma2 = c + d #Suma de cadenas
+
+print ("suma1:", suma1)
+print ("suma2:", suma2)
+
+#3. Lógicos (valores o expresiones booleanas)
+usuario_activo = True
+print ("usuario_activo", type (usuario_activo))
+pago_realizado = False
+print ("pago_realizado", type (pago_realizado))
+
+#4. Listas
+frutas = ['Apple', 'Banana']
+print ("frutas", type (frutas))
+colors = ['blue', 'white', 'green', 'brown']
+print ("colors", type (colors [2]))
+
+#5. Tuplas
+#6. Diccionarios.
+#7. Conjuntos
